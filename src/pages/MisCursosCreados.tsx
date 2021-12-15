79e566a2ffcc8f18df936105bfe57619f4c0a7b0
sasukenny <kennysuarezh@gmail.com>
2021-12-15 11:59:44 -0500

    Update MisCursosCreados.tsx
    consume endpoint

--- src/pages/MisCursosCreados.tsx
+++ src/pages/MisCursosCreados.tsx
@@ -1,3 +1,4 @@
+import { getCourses } from 'api/getCourses';
 import { Curso } from 'common/types';
 import CardCourse from 'components/Card/CardCourse';
 import Wrapper from 'containers/Wrapper';
@@ -7,26 +8,22 @@ const MisCursosCreados = () => {
   const [cursos, setCursos] = useState([]);
   // carga de lista de cursos
   useEffect(() => {
-    (async () => {
-      // Consumir API
-      const headers = {
-        'Content-Type': 'application/json',
-        Authorization: 'Token 476a0d0d4f368cb649935e1ecca5a633cd45d772',
-      };
-      const response = await fetch('http://localhost:8000/courses/created/', {
-        method: 'GET',
-        headers: headers,
+    // Consumir API
+    getCourses()
+      .then((response) => {
+        console.log('data:', response.data);
+        setCursos(response.data);
+      })
+      .catch((e) => {
+        console.error('Error: ', e);
       });
-      const data = await response.json();
-      setCursos(data);
-    })();
   }, []);
   return (
     <Wrapper>
       <div className="album py-5 bg-light">
         <div className="container">
           <div>
-            <h1 className="text-center">Mis Cursos Creados</h1>
+            <h1 className="text-center">Mis Cursos Inscritos</h1>
           </div>
           <div className="row">
             {cursos.map((curso: Curso) => {
