9f9ff1a2a73dfc9d4335b9c752872a9099ad5c52
Miguel Rodriguez <mrodriguezt1203@gmail.com>
2022-01-08 16:03:21 -0500

    FIXES: Course info and base url updated

--- src/pages/CursoDetalles.tsx
+++ src/pages/CursoDetalles.tsx
@@ -10,12 +10,18 @@ const CursoDetalles = () => {
       <div className="col-9 border border-light ">
         <div className="row ">
           <div className="d-flex justify-content-start py-4 border-bottom">
-            <h2>Descripción del curso</h2>
+            <h2>Información del curso</h2>
           </div>
         </div>
         <div className="p-3">
+          <small>
+            <strong>{`${course.day_of_the_week} - ${course.time_start.slice(
+              0,
+              5
+            )} hasta ${course.time_end.slice(0, 5)}`}</strong>
+          </small>
+          <hr />
           <p>{course.description}</p>
-          <Outlet />
         </div>
       </div>
     </>
