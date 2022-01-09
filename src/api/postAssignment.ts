4817d2a40822192e9258c1b4a964df8f8aff795c
Miguel Rodriguez <mrodriguezt1203@gmail.com>
2022-01-08 19:17:48 -0500

    FIXES: refactor

--- src/api/postAssignment.ts
+++ src/api/postAssignment.ts
@@ -5,7 +5,11 @@ export async function postAssignment(id: string, data: newMaterial) {
   const response = await axiosInstance({
     method: 'POST',
     url: `/classroom/${id}/assignments/`,
-    data: { title: data.title, description: data.description },
+    data: {
+      title: data.title,
+      description: data.description,
+      due_datetime: '2022-01-04T21:00:00Z',
+    },
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Token ${localStorage.getItem('user-token')}`,
