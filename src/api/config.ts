6230373b43bddbca7db3d8774d3487e1e30bbca5
unknown <erickp1502@gmail.com>
2022-05-24 20:40:17 -0500

    Identificación de Mejora -Erick palomino

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { BASE_URL } from 'common/constants';
 
+//Crear instancia de Axios por archivo de configuración o de variable de entorno
+//Como se ve a continuación
+/*const axiosInstance = axios.create({
+  baseURL:  process.env.REACT_APP_BASE_URL; 
+});*/
+
 const axiosInstance = axios.create({
   baseURL: BASE_URL || 'http://localhost:8000/',
 });
